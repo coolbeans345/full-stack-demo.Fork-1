1bcbed927ca28abaeaa0ccac64dd332942afe718
Nish Shank <nishtih.shankar@gmail.com>
2022-07-09 17:03:08 -0700

    This is Nishith's project with updates (photo and description about myself)

--- front-end/src/Aboutme.js
+++ front-end/src/Aboutme.js
@@ -1,4 +1,6 @@
 import './aboutme.css'
+import photo from './nishiths.jpg'
+import { Link } from 'react-router-dom'
 
 /**
  * A React component that represents the Home page of the app.
@@ -8,13 +10,18 @@ import './aboutme.css'
 const Aboutme = props => {
   return (
     <>
-      <h1>Hi there, I'm Sid</h1>
-      <p>I'm a second-year undergraduate student from New York University.</p>
-      <p>
-      I chose to major in computer science because it affords me the opportunity to explore my passion for solving problems with my hand. I love to engage in this end-to-end experience which is fulfilling to me because I get to not only brings up an idea but also bring it to fruition.
+      <h1>Hi there, I'm Nishith</h1>
+      <p>I'm a student about to go to my Junior year and I am going to Wilcox highschool</p>
+      <p> I enjoy programming and working with computers. In my free time, I like to read and play my guitar. Some sports I play include Cross Country and Track and Field. My dream college is UCLA. 
       </p>
-    </>
-  )
+      <nav className="Header-navbar">
+      <Link to="/" className="photo">
+        <img src={photo} alt="Our fabulous photo" />
+        </Link>
+        </nav>
+        
+    </>  
+    )
 }
 
 // make this component available to be imported into any other file
